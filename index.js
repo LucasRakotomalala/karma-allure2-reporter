528537c73130573f31ebf9c47e830bd69802e649
Lucas Rakotomalala <lucas.rakotomalala@yahoo.fr>
2025-01-13 16:59:29 +0100

    chore: add files

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const AllureReporter = require('./src/KarmaAllureReporter');
+
+module.exports = {
+  'reporter:allure': ['type', AllureReporter]
+};
---
+++ src/karma-allure2-reporter.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const { Stage, Status } = require('allure-js-commons');
+
+const {
+  createDefaultWriter,
+  ReporterRuntime,
+  getEnvironmentLabels,
+  getLanguageLabel,
+  getFrameworkLabel,
+  getHostLabel,
+  getThreadLabel,
+} = require('allure-js-commons/sdk/reporter');
+
+function KarmaAllureReporter(baseReporterDecorator, config, logger) {
+  const log = logger.create('KarmaAllureReporter');
+
+  const { resultsDir = 'allure-results', ...restOptions } = config.allureReporter || {};
+
+  const allureRuntime = new ReporterRuntime({
+    writer: createDefaultWriter({ resultsDir }),
+    ...restOptions
+  });
+
+  let currentTestUuid = null;
+  const scopeStack = [];
+
+  baseReporterDecorator(this);
+
+  this.onSpecComplete = function (browser, result) {
+    log.debug('Spec complete: ', result);
+
+    const testName = result.description; // Represents the `it` test name
+    const parentSuite = result.suite.join(' - '); // Concatenated `describe` texts separated by '-'
+    const packageName = result.suite.join('.'); // Concatenated `describe` texts separated by '.'
+
+    // Retrieve global and initial labels
+    const globalLabels = getEnvironmentLabels().filter((label) => !!label.value);
+    const initialLabels = [
+      getLanguageLabel(),
+      getFrameworkLabel(getFrameworkName()),
+      getHostLabel(),
+      getThreadLabel(process.pid), // Use the current process ID for the thread label
+    ];
+
+    if (!currentTestUuid) {
+      const scopeUuid = allureRuntime.startScope();
+      scopeStack.push(scopeUuid);
+
+      currentTestUuid = allureRuntime.startTest(
+        {
+          name: testName,
+          fullName: `${parentSuite} ${testName}`,
+          stage: Stage.RUNNING,
+          labels: [
+            ...globalLabels,
+            ...initialLabels,
+            { name: 'browser', value: browser.name }, // Add the browser label
+            { name: 'package', value: packageName }, // Add the package name label
+            { name: 'parentSuite', value: parentSuite }, // Add the parent suite label
+            { name: 'suite', value: testName }, // Add the suite label
+          ],
+        },
+        scopeStack
+      );
+    }
+
+    // Update the test status and details
+    allureRuntime.updateTest(currentTestUuid, (test) => {
+      if (result.success) {
+        test.status = Status.PASSED;
+        test.stage = Stage.FINISHED;
+      } else if (result.skipped) {
+        test.status = Status.SKIPPED;
+        test.stage = Stage.PENDING;
+        test.statusDetails = {
+          message: 'Test skipped',
+        };
+      } else {
+        test.status = Status.FAILED;
+        test.stage = Stage.FINISHED;
+        test.statusDetails = {
+          message: result.log.join('\n'),
+        };
+      }
+    });
+
+    finalizeTest();
+  };
+
+  this.onRunComplete = function () {
+    finalizeScopes();
+  };
+
+  function finalizeTest() {
+    if (currentTestUuid) {
+      allureRuntime.stopTest(currentTestUuid);
+      allureRuntime.writeTest(currentTestUuid);
+      currentTestUuid = null;
+    }
+
+    const scopeUuid = scopeStack.pop();
+    if (scopeUuid) {
+      allureRuntime.writeScope(scopeUuid);
+    }
+  }
+
+  function finalizeScopes() {
+    while (scopeStack.length > 0) {
+      const scopeUuid = scopeStack.pop();
+      allureRuntime.writeScope(scopeUuid);
+    }
+  }
+
+  function getFrameworkName() {
+    return 'karma'; // Returns the name of the testing framework
+  }
+}
+
+KarmaAllureReporter.$inject = ['baseReporterDecorator', 'config', 'logger'];
